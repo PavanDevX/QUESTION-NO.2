d455ccc25884fbd84e5bfb41ee235f1293559b24
PAVAN KUMAR <pavankumar_doppalapudi@srmap.edu.in>
2025-04-25 17:27:51 +0530

    Create serInfo.java

---
+++ serInfo.java
@@ -0,0 +1,16 @@
+package com.example.bajaj.model;
+
+public class UserInfo {
+    private String name;
+    private String regNo;
+    private String email;
+
+    public UserInfo() {}
+    public UserInfo(String name, String regNo, String email) {
+        this.name = name;
+        this.regNo = regNo;
+        this.email = email;
+    }
+
+    // Getters and setters
+}
