ccdac315d38bf79807bce1e7f455053048c7f68d
PAVAN KUMAR <pavankumar_doppalapudi@srmap.edu.in>
2025-04-25 17:29:42 +0530

    Create ResultQ2.java

---
+++ ResultQ2.java
@@ -0,0 +1,15 @@
+package com.example.bajaj.model;
+
+import java.util.List;
+
+public class ResultQ2 {
+    private String regNo;
+    private List<Integer> outcome;
+
+    public ResultQ2(String regNo, List<Integer> outcome) {
+        this.regNo = regNo;
+        this.outcome = outcome;
+    }
+
+    // Getters and setters
+}
